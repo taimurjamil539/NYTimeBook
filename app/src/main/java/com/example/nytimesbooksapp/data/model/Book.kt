a2611683e721403a99fee1d7c6f9887592985a1a
taimurjamil539 <taimurjamil539@gmail.com>
2025-10-30 16:59:30 +0500

    Chnage in BookDTO

--- app/src/main/java/com/example/nytimesbooksapp/data/model/Book.kt
+++ app/src/main/java/com/example/nytimesbooksapp/data/model/Book.kt
@@ -4,23 +4,11 @@ data class Book(
 
     val author: String,
     val book_image: String,
-
     val buy_links: List<BuyLink>,
-//    val contributor: String,
-//    val contributor_note: String,
     val created_date: String,
-//    val dagger: Int,
     val description: String,
-//    val first_chapter_link: String,
-//    val isbns: List<Isbn>,
-//    val price: String,
-//    val primary_isbn10: String,
-//    val primary_isbn13: String,
     val publisher: String,
     val rank: Int,
-//    val rank_last_week: Int,
-//    val sunday_review_link: String,
     val title: String,
-//    val updated_date: String,
-//    val weeks_on_list: Int
+
 )
